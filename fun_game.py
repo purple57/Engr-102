5113213b853b39dd46c12dcc9ba8b09b9274b37b
purple57 <89474247+purple57@users.noreply.github.com>
2025-11-21 23:44:59 -0600

    Create fun_game.py

---
+++ fun_game.py
@@ -0,0 +1,10 @@
+#By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment"
+# Names: Ismael Ortega
+# Manning Phan
+# Caitlyn Brown
+# Anita Kumar
+# Section: 504 & 404
+# Assignment: Chess game - Chapter 13 Lab
+# Date: 11/20/25
