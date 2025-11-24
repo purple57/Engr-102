8d56a676490e40a27df3410e3bf416f112d57b8d
BBerris <83521697+BBerris@users.noreply.github.com>
2025-11-24 15:50:03 -0600

    Create Screen

--- fun_game.py
+++ fun_game.py
@@ -10,3 +10,40 @@
 # Date: 11/20/25
 
 import numpy as np
+import pygame
+
+pygame.init()
+W, H = 600, 600
+screen = pygame.display.set_mode((900, 900))
+#pygame.draw(0, 0, W, H)
+
+square = pygame.Rect(0, 0, 50, 50)
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, (0,0,0), square)
+
+
+    
+    
+
+
+
+
+
+boardsize = [["R","Kn","B","K","Q","B","Kn","R"],
+             ["P","P","P","P","P","P","P","P"],
+             ["","","","","","","",""],
+             ["","","","","","","",""],
+             ["","","","","","","",""],
+             ["","","","","","","",""],
+             ["P","P","P","P","P","P","P","P"],
+             ["R","Kn","B","K","Q","B","Kn","R"]]
