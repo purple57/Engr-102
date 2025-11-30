c7ebdc5f519678731212a960c4371881cdbdf75b
BBerris <83521697+BBerris@users.noreply.github.com>
2025-11-30 10:34:54 -0600

    Update fun_game.py

--- fun_game.py
+++ fun_game.py
@@ -23,26 +23,11 @@ def main():
     blpawn_img = pygame.image.load("C:\\Users\\Manning\\Downloads\\blackpawn.png").convert_alpha()
     blpawn1 = boardfunc.Pieces((Fuckaround.mydict["a7"]), blpawn_img)
     
-    clicking = False
+    clicking = True
 
     running = True
 
     pygame.draw.rect(window,(50,50,50),(0,0,1000,800))
-    for i in Fuckaround.mydict:
-        if "a" in i or "c" in i or "e" in i or "g" in i:
-            if count%2 == 0:
-                pygame.draw.rect(window, (197,140,99), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
-            else:
-                pygame.draw.rect(window, (250,236,210), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
-            count += 1
-        elif "b" in i or "d" in i or "f" in i or "h" in i:
-            if count%2 != 0:
-                pygame.draw.rect(window, (197,140,99), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
-            else:
-                pygame.draw.rect(window, (250,236,210), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
-            count += 1
-    
-    blpawn1.draw(window)
 
 
     while running:
@@ -52,13 +37,28 @@ def main():
                 running = False
                 break
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if clicking == True:
-                    pygame.draw.rect(window,(195,186,191),(blpawn1.rect.x, blpawn1.rect.y + 75, 50, 50))
-                    clicking = False
-                elif clicking == False:
+                clicking = not clicking
+                
+        for i in Fuckaround.mydict:
+            if "a" in i or "c" in i or "e" in i or "g" in i:
+                if count%2 == 0:
+                    pygame.draw.rect(window, (197,140,99), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
+                else:
+                    pygame.draw.rect(window, (250,236,210), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
+                count += 1
+            elif "b" in i or "d" in i or "f" in i or "h" in i:
+                if count%2 != 0:
+                    pygame.draw.rect(window, (197,140,99), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
+                else:
+                    pygame.draw.rect(window, (250,236,210), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
+                count += 1
+    
+        blpawn1.draw(window)
                     
-                    clicking = True
-                print("clicked")
+        if clicking:
+            pygame.draw.rect(window,(195,186,191), placementsquare)
+            print("clicked")
+        
         pygame.display.flip()
 
     pygame.quit()
