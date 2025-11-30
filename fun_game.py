0bf416fc55893252eec25aae57b25d94dd3add36
BBerris <83521697+BBerris@users.noreply.github.com>
2025-11-30 00:45:45 -0600

    Update fun_game.py

--- fun_game.py
+++ fun_game.py
@@ -12,26 +12,56 @@ import pygame
 import boardfunc
 
 def main():
+    count = 1
+    
+    boardfunc.board()
+    boardfunc.beginplacement()
     pygame.init()
+    
     window = pygame.display.set_mode((1000,800))
+    
+    blpawn_img = pygame.image.load("C:\\Users\\Manning\\Downloads\\blackpawn.png").convert_alpha()
+    blpawn1 = boardfunc.Pieces((Fuckaround.mydict["a7"]), blpawn_img)
+    
+    clicking = False
 
     running = True
+
+    pygame.draw.rect(window,(50,50,50),(0,0,1000,800))
+    for i in Fuckaround.mydict:
+        if "a" in i or "c" in i or "e" in i or "g" in i:
+            if count%2 == 0:
+                pygame.draw.rect(window, (197,140,99), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
+            else:
+                pygame.draw.rect(window, (250,236,210), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
+            count += 1
+        elif "b" in i or "d" in i or "f" in i or "h" in i:
+            if count%2 != 0:
+                pygame.draw.rect(window, (197,140,99), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
+            else:
+                pygame.draw.rect(window, (250,236,210), (Fuckaround.mydict[i][0]-50/4, Fuckaround.mydict[i][1]-50/4, 75, 75))
+            count += 1
+    
+    blpawn1.draw(window)
+
+
     while running:
+        pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 break
-        pygame.draw.rect(window,(50,50,50),(0,0,1000,800))
-
-        boardfunc.board(window,50,50)
-        boardfunc.beginplacement(window, boardfunc.mydict)
-        #Middle = half of screen - half of rect size
-
-
-        pygame.display.update()    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if clicking == True:
+                    pygame.draw.rect(window,(195,186,191),(blpawn1.rect.x, blpawn1.rect.y + 75, 50, 50))
+                    clicking = False
+                elif clicking == False:
+                    
+                    clicking = True
+                print("clicked")
+        pygame.display.flip()
 
     pygame.quit()
 
 if __name__ == "__main__":
     main()
-    print(boardfunc.mydict)
