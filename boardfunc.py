71b8c36ad4b0afd797ec1d11895e0161cda3684c
BBerris <83521697+BBerris@users.noreply.github.com>
2025-11-30 00:44:52 -0600

    Update boardfunc.py

--- boardfunc.py
+++ boardfunc.py
@@ -1,63 +1,88 @@
+
 import pygame
 mydict={}
+pieceonboard = {}
 whitelist = ["a1", "a2", "b1", "b2", "c1", "c2", "d1", "d2", "e1", "e2", "f1", "f2", "g1", "g2", "h1", "h2"]
 blaclist = ["a7", "a8", "b7", "b8", "c7", "c8", "d7", "d8", "e7", "e8", "f7", "f8", "g7", "g8", "h7", "h8"]
-def board(window,sizex,sizey):
-    count = 1
+def board(sizex=50,sizey=50):
     x = 200
     y = 100
     for i in range(8):
         if i % 2 == 0:
             for j in range(8):
-                if count % 2 == 0:
-                    pygame.draw.rect(window,(197,140,99),(x+i*75,y+j*75,75,75))
-                else:
-                    pygame.draw.rect(window,(250,236,210),(x+i*75,y+j*75,75,75))
                 if i == 0:
-                    mydict[f"a{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"a{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 1:
-                    mydict[f"b{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"b{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 2:
-                    mydict[f"c{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"c{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 3:
-                    mydict[f"d{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"d{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 4:
-                    mydict[f"e{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"e{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 5:
-                    mydict[f"f{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"f{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 6:
-                    mydict[f"g{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"g{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 7:
-                    mydict[f"h{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
-                count += 1
+                    mydict[f"h{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
         else:
             for j in range(8):
-                if count % 2 != 0:
-                    pygame.draw.rect(window,(197,140,99),(x+i*75,y+j*75,75,75))
-                else:
-                    pygame.draw.rect(window,(250,236,210),(x+i*75,y+j*75,75,75))
                 if i == 0:
-                    mydict[f"a{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"a{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 1:
-                    mydict[f"b{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"b{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 2:
-                    mydict[f"c{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"c{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 3: 
-                    mydict[f"d{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"d{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 4:
-                    mydict[f"e{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"e{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 5:
-                    mydict[f"f{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"f{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 6:
-                    mydict[f"g{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
+                    mydict[f"g{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
                 elif i == 7:
-                    mydict[f"h{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4, sizex, sizey)
-                count += 1
+                    mydict[f"h{j+1}"] = ((x+i*75)+sizex/4,(y+j*75)+sizey/4)
     return True
 
-def beginplacement(window, diction):
+def beginplacement():
     for i in whitelist:
-        pygame.draw.rect(window,(195,186,191),(diction[i]))
+        if "2" in i:
+            pieceonboard[i] = "white pawn"
+        elif "a1" == i or "h1" == i:
+            pieceonboard[i] = "white rook"
+        elif "b1" == i or "g1" == i:
+            pieceonboard[i] = "white knight"
+        elif "c1" == i or "f1" == i:
+            pieceonboard[i] = "white bishop"
+        elif "d1" == i:
+            pieceonboard[i] = "white queen"
+        elif "e1"== i:
+            pieceonboard[i] = "white king"
     for i in blaclist:
-        pygame.draw.rect(window,(30,30,30),(diction[i]))
+        if "7" in i:
+            pieceonboard[i] = "black pawn"
+        elif "a8" == i or "h8" == i:
+            pieceonboard[i] = "black rook"
+        elif "b8" == i or "g8" == i:
+            pieceonboard[i] = "black knight"
+        elif "c8" == i or "f8" == i:
+            pieceonboard[i] = "black bishop"
+        elif "d8" == i:
+            pieceonboard[i] = "black queen"
+        elif "e8"== i:
+            pieceonboard[i] = "black king"
     return True
+
+class Pieces():
+    
+    def __init__(self,post,image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = post
+
+
+    def draw(self,window):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+        #Draw pieces
